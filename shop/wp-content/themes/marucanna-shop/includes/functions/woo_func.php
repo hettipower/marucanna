969dc9bbbe387d1774bc88ba6a273849300a05a3
TharinduH <hettipower@gmail.com>
2024-02-20 01:08:41 +0530

    update

--- shop/wp-content/themes/marucanna-shop/includes/functions/woo_func.php
+++ shop/wp-content/themes/marucanna-shop/includes/functions/woo_func.php
@@ -2,6 +2,7 @@
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 6 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 5 );
