a23de87a388e7d1cdaa190f05f008bfe2d8e6f88
TharinduH <hettipower@gmail.com>
2024-09-12 02:40:01 +0530

    updated

--- wp-content/themes/marucanna/header.php
+++ wp-content/themes/marucanna/header.php
@@ -66,6 +66,20 @@ if( $source_type != 'none' && isset($source_type) ) {
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenuWrap" aria-controls="mainMenuWrap" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
+			<div class="mobile_btns">
+				<a href="<?php echo home_url('check-eligibility'); ?>/" class="btn style_1">CHECK ELIGIBILITY</a>
+				<?php 
+					if (is_user_logged_in()): 
+						$user = wp_get_current_user();
+    					$allowed_roles = array( 'patient', 'administrator' );
+						if ( array_intersect( $allowed_roles, $user->roles ) ) :
+				?>
+					<a href="<?php echo home_url('patient-dashboard'); ?>/" class="btn style_2">Patient Dashboard</a>
+					<?php endif; ?>
+				<?php else: ?>
+					<a href="<?php echo home_url('login'); ?>/" class="btn style_2">Login</a>
+				<?php endif; ?>
+			</div>
 			<div class="collapse navbar-collapse" id="mainMenuWrap">
 				<?php
 					wp_nav_menu( array(
@@ -85,20 +99,6 @@ if( $source_type != 'none' && isset($source_type) ) {
 					<?php endif; ?>
 				<?php else: ?>
 					<a href="<?php echo home_url('login'); ?>/" class="btn style_2">REPEAT ORDER</a>
-				<?php endif; ?>
-			</div>
-			<div class="btn-group mobile_btns">
-				<a href="<?php echo home_url('check-eligibility'); ?>/" class="btn style_1">CHECK ELIGIBILITY</a>
-				<?php 
-					if (is_user_logged_in()): 
-						$user = wp_get_current_user();
-    					$allowed_roles = array( 'patient', 'administrator' );
-						if ( array_intersect( $allowed_roles, $user->roles ) ) :
-				?>
-					<a href="<?php echo home_url('patient-dashboard'); ?>/" class="btn style_2">Patient Dashboard</a>
-					<?php endif; ?>
-				<?php else: ?>
-					<a href="<?php echo home_url('login'); ?>/" class="btn style_2">Login</a>
 				<?php endif; ?>
 			</div>
 		</div>
